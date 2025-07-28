3f77df638e216de10605fe13a1006a5d5b4964c8
Mazhar1857 <ali.mazhar1857@gmail.com>
2025-07-28 17:07:28 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import HomePage from "./component/HomePage";
 import Setting from "./component/Setting";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const theme = useSelector((store) => store.theme);
   const font = useSelector((store) => store.font);
@@ -11,10 +12,22 @@ function App() {
   };
 
   return (
-    <div className="app" style={style}>
-      <HomePage />
-      <Setting />
-    </div>
+    <Router>
+      <div className="app" style={style}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route
+            path="/setting"
+            element={
+              <div className="setting-component">
+                <Setting />
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
