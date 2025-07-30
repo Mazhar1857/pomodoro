adf0a7cf8cbf93b82fbfa180d8c3512d270622a3
Mazhar1857 <ali.mazhar1857@gmail.com>
2025-07-30 15:09:48 +0530

    add timer into the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./themeSlice";
 import fontSlice from "./fontSlice";
+import timerSlice from "./timerSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeSlice.reducer,
     font: fontSlice.reducer,
+    timer: timerSlice.reducer,
   },
 });
 
