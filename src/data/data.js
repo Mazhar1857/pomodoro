11d53890202410f306fcb444ed95825656c412be
Mazhar1857 <ali.mazhar1857@gmail.com>
2025-08-04 15:51:12 +0530

    add data.js file

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const pomodoroTimers = {
+  pomodoro: {
+    min: 0,
+    sec: 30,
+    totalSecond: 30,
+  },
+  shortBreak: {
+    min: 0,
+    sec: 10,
+    totalSecond: 10,
+  },
+  longBreak: {
+    min: 0,
+    sec: 20,
+    totalSecond: 20,
+  },
+  round: 2,
+};
